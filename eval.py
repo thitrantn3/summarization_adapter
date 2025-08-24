7579d270f8a9b85885a565ad68b3c707e75fbe25
Tran Ngoc Nha Thi <80879182+thitrantn3@users.noreply.github.com>
2025-08-24 22:34:25 +0800

    test evaluation 1

--- eval.py
+++ eval.py
@@ -1,33 +1,50 @@
 import torch
 from transformers import AutoTokenizer
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from peft import PeftModel
-from model import get_lora_model
-from dataset import load_cnn_dailymail_dataset
-from rouge_score import rouge_scorer
-
-# Load tokenizer
-MODEL_NAME = "Qwen/Qwen2.5-3B"
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-tokenizer.pad_token = tokenizer.eos_token
-
-# Load LoRA model
-base_model = get_lora_model()  # returns base + LoRA applied
-base_model.eval()
+from config import MODEL_NAME
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from evaluate import load
+import evaluate
+import numpy as np
 
 # Load validation dataset
-val_dataset = load_cnn_dailymail_dataset(split="validation")
+tokenized_val = load_from_disk("./t_data/tokenized_val")
+r_val = load_from_disk("./r_data/r_val")
+print(f'Loaded eval dataset length: {len(tokenized_val)}, {len(r_val)}')
+
+# Load trained model
+MODEL_PATH = "./peft-text-summary"  # path where you saved your PEFT model
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load base model (same as the one you trained on)
+base_model_name = MODEL_NAME  # or your original base model
+model = AutoModelForCausalLM.from_pretrained(
+    base_model_name,
+    device_map={"": device},        # automatically put layers on GPU
+    torch_dtype="auto"        # or torch.float16
+)
+
+# Load PEFT / LoRA adapters
+ft_model = PeftModel.from_pretrained(model, MODEL_PATH)
+model.eval()
 
 # Evaluation function
-def evaluate_model(model, dataset, tokenizer):
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
-    rouge1_scores, rouge2_scores, rougeL_scores = [], [], []
+def evaluate_model(model, t_dataset, r_dataset, tokenizer):
 
-    for sample in dataset:
-        input_ids = sample['input_ids'].unsqueeze(0).to(model.device)
-        attention_mask = sample['attention_mask'].unsqueeze(0).to(model.device)
+    res = []
+
+    for i in range(len(t_dataset)):
+        # print(f"Prompt: {r_val[i]['text']}")
+        # print(f"Human-generated summary: {r_val[i]['text'].split('Response:\n')[1]}")
+        t_sample = tokenized_val[i]
+        input_ids = t_sample['input_ids'].unsqueeze(0).to(model.device)
+        attention_mask = t_sample['attention_mask'].unsqueeze(0).to(model.device)
 
         # Generate summary
+        print(f"Generating summary {i}....")
         with torch.no_grad():
             outputs = model.generate(
                 input_ids=input_ids,
@@ -37,16 +54,36 @@ def evaluate_model(model, dataset, tokenizer):
             )
 
         pred = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        ref = sample['highlights']
+        pred = pred.split("Response:\n")[1].strip() if "Response:\n" in pred else ""
+        print(f"PREDICTION: {len(pred)}")
+        # print(f"PREDICTION: {len(pred)}")
+        # print(f"PREDICTION WITHOUT INPUT with {len(pred)}: {pred[1]}")
 
-        scores = scorer.score(ref, pred)
-        rouge1_scores.append(scores['rouge1'].fmeasure)
-        rouge2_scores.append(scores['rouge2'].fmeasure)
-        rougeL_scores.append(scores['rougeL'].fmeasure)
+        r_sample = r_val[i]
+        ref = r_sample.get('highlights')
+        # print(ref)
+        print(f'ref length: {len(ref)}')
 
-    print(f"ROUGE-1: {sum(rouge1_scores)/len(rouge1_scores):.4f}")
-    print(f"ROUGE-2: {sum(rouge2_scores)/len(rouge2_scores):.4f}")
-    print(f"ROUGE-L: {sum(rougeL_scores)/len(rougeL_scores):.4f}")
+        if len(pred) == 0 or len(ref) == 0:
+          print("No predictions or references to compute ROUGE.")    
+          res.append({
+              'rouge1': np.float64(0.0),
+              'rouge2': np.float64(0.0),
+              'rougeL': np.float64(0.0),
+              'rougeLsum': np.float64(0.0)
+          })
+          continue
+
+        print("Calculating rouge scores...")
+        rouge = evaluate.load('rouge')
+        results = rouge.compute(predictions=pred,
+                            references=ref[:len(pred)],
+                            tokenizer=lambda x: x.split(),
+                            use_aggregator=True,
+                            use_stemmer=True)
+        res.append(results)
+
+        return res
 
 # Run evaluation
-evaluate_model(base_model, val_dataset, tokenizer)
+results = evaluate_model(model, tokenized_val, r_val, tokenizer)
